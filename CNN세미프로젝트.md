2c83eb949cd66b5b28345eedff5fb0d983182637
naxvinci <nowney@naver.com>
2020-04-17 09:15:27 +0900

    20200417 | study1

---
+++ CNN세미프로젝트.md
@@ -0,0 +1,10 @@
+Google Search API
+
+GCP(Google Cloud Platform)
+
+API Key값 생성
+
+AIzaSyDpIHj3MbJqh35O1dylsGFPI8LoOUS5eJk
+
+
+
---
+++ cnn_project/02.img_face_detector_practice_.py
@@ -0,0 +1,22 @@
+import os
+import pathlib
+import glob
+import cv2 
+import settings
+
+def load_name_images(image_path_pattern):
+    return
+
+def detect_image_face(file_path, image,cascade_filepath):
+    return
+
+def delete_dir(dir_path, is_delete_top_dir=True):
+    return
+
+def main():
+    return
+
+if __name__ = "__main__":
+    main()
+
+    
